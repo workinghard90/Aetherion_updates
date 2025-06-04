59ba5e80ed08edf8d7b5efe2d8fa17eee1d06535
Autumn Hall <autumnmhall90@gmail.com>
2025-06-04 14:41:58 -0700

    Delete backend/launchpad directory

--- backend/launchpad/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route("/launchpad/hello", methods=["GET"])
-def hello_launchpad():
-    return jsonify({"msg": "Hello from LaunchPad!"})
-
-if __name__ == "__main__":
-    app.run(port=5001, debug=True)
