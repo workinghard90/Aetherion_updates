51489362cf99c9e21d6f55937fd08d34c81657a1
Autumn Hall <autumnmhall90@gmail.com>
2025-06-06 17:56:07 -0400

    Delete backend/scrolls.py

--- backend/scrolls.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from .models import Scroll
-
-bp = Blueprint("scrolls", __name__)
-
-@bp.route("/", methods=["GET"])
-def get_all_scrolls():
-    scrolls = Scroll.query.all()
-    return jsonify([{"id": s.id, "title": s.title} for s in scrolls])
